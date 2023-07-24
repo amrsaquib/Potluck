48adb114f783004a98b7b34b9074c9c0448eebeb
amrsaquib <amrsaquib@gmail.com>
2023-07-24 15:59:12 -0400

    vendor list complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import Header from './Components/Header/Header';
 import Vendor from './Components/Vendor/Vendor';
+import VendorsList from './Components/VendorsList/VendorsList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Vendor />
+      <VendorsList />
     </div>
   );
 }
---
+++ src/Components/VendorsList/VendorsList.jsx
@@ -0,0 +1,16 @@
+import './VendorsList.scss'
+import Vendor from '../Vendor/Vendor'
+
+export default function VendorsList() {
+    return (
+        <div className='vendor-list'>
+            <Vendor />
+            <Vendor />
+            <Vendor />
+            <Vendor />
+            <Vendor />
+            <Vendor />
+            <Vendor />
+        </div>
+    )
+}
